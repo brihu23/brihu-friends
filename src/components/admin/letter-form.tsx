d8ade3b97f1567fd0149e216a8c6cec04a66fdbf
brihu.sundararaman@yale.edu <brihu.sundararaman@yale.edu>
2024-01-04 20:15:40 -0500

    updates

--- src/components/admin/letter-form.tsx
+++ src/components/admin/letter-form.tsx
@@ -4,25 +4,230 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import ModeToggle from "@/components/ui/mode-toggle";
 import { Combobox } from "../ui/combobox";
+import { useState } from "react";
+import {
+  ArrowDownSquare,
+  ArrowUpSquare,
+  MinusSquare,
+  PlusSquare,
+} from "lucide-react";
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
+import { cn } from "@/lib/utils";
+import { set } from "date-fns";
+
+type TContent = {
+  imageUrl?: string;
+  imageCaption?: string;
+  text?: string;
+  type: "image" | "text";
+};
 
 export default function LetterForm() {
+  const [letterState, setLetterState] = useState<{
+    personId: string | null;
+    titles: string[];
+    content: TContent[];
+    displayDate: string;
+  }>({
+    personId: null,
+    titles: [""],
+    content: [
+      {
+        type: "text",
+        text: "",
+      },
+    ],
+    displayDate: "",
+  });
   return (
     <div className="flex flex-col justify-start items-start w-2/5 space-y-3">
       <h1 className="text-2xl my-2">New Letter</h1>
       <div className="grid w-full  items-center gap-1.5">
-        <Label htmlFor="First Name">Letter For</Label>
-        <Combobox itemName="Person" itemNamePlural="People"/>
+        <Label className="text-md" htmlFor="First Name">
+          Letter For
+        </Label>
+        <Combobox itemName="Person" itemNamePlural="People" />
       </div>
       <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="Title">Title</Label>
-        <Input type="text" id="Title" placeholder="Title" />
+        <Label
+          className="flex justify-between items-center my-4 text-md"
+          htmlFor="Title"
+        >
+          Title(s){" "}
+          <PlusSquare
+            className="cursor-pointer hover:scale-110 transition-all duration-300"
+            onClick={() => {
+              setLetterState((prevState) => ({
+                ...prevState,
+                titles: [...prevState.titles, ""],
+              }));
+            }}
+          />
+        </Label>
+        {letterState.titles.map((title, index) => {
+          return (
+            <div className="flex items-center justify-between" key={index}>
+              <Input
+                type="text"
+                id="Title"
+                placeholder={`Title`}
+                value={title}
+                onChange={(e) => {
+                  setLetterState((prevState) => {
+                    const titles = [...prevState.titles];
+                    titles[index] = e.target.value;
+                    return { ...prevState, titles };
+                  });
+                }}
+              />{" "}
+              <MinusSquare
+                className=" ml-2 cursor-pointer hover:scale-110 transition-all duration-300"
+                onClick={() => {
+                  setLetterState((prevState) => {
+                    const titles = [...prevState.titles].filter(
+                      (_, i) => i !== index
+                    );
+                    console.log(titles);
+                    return { ...prevState, titles };
+                  });
+                }}
+              />
+            </div>
+          );
+        })}
       </div>
       <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="Password">Content </Label>
-        <Input type="text" id="Password" placeholder="Password" />
+        <div className="flex justify-between items-center my-4">
+          <Label className="text-md">Content </Label>
+          <div className="flex  space-x-4 text-sm">
+            {" "}
+            <span className="flex">
+              Text{" "}
+              <PlusSquare
+                className={cn("ml-2 cursor-pointer hover:scale-110 transition-all duration-300")}
+                onClick={() => {
+                  setLetterState((prevState) => {
+                    const content = [...prevState.content];
+                    content.push({ type: "text" });
+                    return { ...prevState, content };
+                  });
+                }}
+              />
+            </span>
+            <span className="flex">
+              Image{" "}
+              <PlusSquare
+                className={cn("ml-2 cursor-pointer hover:scale-110 transition-all duration-300")}
+                onClick={() => {
+                  setLetterState((prevState) => {
+                    const content = [...prevState.content];
+                    content.push({ type: "image" });
+                    return { ...prevState, content };
+                  });
+                }}
+              />
+            </span>
+          </div>
+        </div>
+
+        {letterState.content.map((content, index) => {
+          return (
+            <div className="flex items-center w-full mb-4">
+              {content.type === "image" ? (
+                <div className="flex flex-col items-start w-full">
+                  <Input
+                    type="text"
+                    placeholder={`Image Url`}
+                    value={content.imageUrl}
+                    onChange={(e) => {
+                      setLetterState((prevState) => {
+                        const content = [...prevState.content];
+                        content[index].imageUrl = e.target.value;
+                        return { ...prevState, content };
+                      });
+                    }}
+                  />
+                  <Input
+                    type="text"
+                    className="mt-1"
+                    placeholder={`Image Caption`}
+                    value={content.imageUrl}
+                    onChange={(e) => {
+                      setLetterState((prevState) => {
+                        const content = [...prevState.content];
+                        content[index].imageCaption = e.target.value;
+                        return { ...prevState, content };
+                      });
+                    }}
+                  />
+                </div>
+              ) : (
+                <Textarea
+                  placeholder={`Text`}
+                  value={content.text}
+                  onChange={(e) => {
+                    setLetterState((prevState) => {
+                      const content = [...prevState.content];
+                      content[index].text = e.target.value;
+                      return { ...prevState, content };
+                    });
+                  }}
+                />
+              )}
+              <div className="flex flex-col gap-x-12">
+                {" "}
+                <ArrowUpSquare
+                  className={cn(
+                    "ml-2 cursor-pointer hover:scale-110 transition-all duration-300",
+                    index === 0 && "opacity-50"
+                  )}
+                  onClick={() => {
+                    if (index === 0) return;
+                    setLetterState((prevState) => {
+                      const content = [...prevState.content];
+                      const temp = content[index];
+                      content[index] = content[index - 1];
+                      content[index - 1] = temp;
+                      return { ...prevState, content };
+                    });
+                  }}
+                />
+                <ArrowDownSquare
+                  className={cn(
+                    "ml-2 cursor-pointer hover:scale-110 transition-all duration-300",
+                    index === letterState.content.length - 1 && "opacity-50"
+                  )}
+                  onClick={() => {
+                    if (index === letterState.content.length - 1) return;
+                    setLetterState((prevState) => {
+                      const content = [...prevState.content];
+                      const temp = content[index];
+                      content[index] = content[index + 1];
+                      content[index + 1] = temp;
+                      return { ...prevState, content };
+                    });
+                  }}
+                />
+                <MinusSquare
+                  className=" ml-2 cursor-pointer hover:scale-110 transition-all duration-300"
+                  onClick={() => {
+                    setLetterState((prevState) => {
+                      const content = [...prevState.content].filter(
+                        (_, i) => i !== index
+                      );
+                      console.log(content);
+                      return { ...prevState, content };
+                    });
+                  }}
+                />
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="Display Date">Display Date</Label>
+      <div className="grid w-full items-center gap-1.5 mt-4">
+        <Label className='text-md'htmlFor="Display Date">Display Date</Label>
         <Input type="text" id="Display Date" placeholder="Display Date" />
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,77 +1,57 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        flicker: {
+          "0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%": {
+            opacity: '0.99',
+            filter:
+              "drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))",
+          },
+          "20%, 21.999%, 63%, 63.999%, 65%, 69.999%": {
+            opacity: '0.4',
+            filter: "none",
+          },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        shimmer: {
+          "0%": {
+            backgroundPosition: "-700px 0",
+          },
+          "100%": {
+            backgroundPosition: "700px 0",
+          },
+        },
+        fadeIn: {
+          "0%": {
+            opacity: '0',
+          },
+          "100%": {
+            opacity: '1',
+          },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        flicker: "flicker 3s linear infinite",
+        shimmer: "shimmer 1.3s linear infinite",
+        "fade-in": "fadeIn 1s ease-in-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  // corePlugins: {
+  //   preflight: false,
+  // },
+  plugins: [require("tailwindcss-inner-border")],
+};
+export default config;
